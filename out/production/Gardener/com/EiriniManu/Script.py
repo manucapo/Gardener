33479a8078c3366afc9f8e96bb5f5da66d8ad868
manucapo <manolotelleria@hotmail.com>
2021-11-22 15:29:01 +0100

    small commit

--- out/production/Gardener/com/EiriniManu/Script.py
+++ out/production/Gardener/com/EiriniManu/Script.py
@@ -5,7 +5,7 @@ import os
 def generateFile(path, implementingClass, methodName):
  tempdiag = open(path, 'w+')
  tempdiag.write('@startuml \n')
- tempdiag.write(implementingClass + ' -> Bob: ' + methodName + '\n')
+ tempdiag.write(' [o-> '+implementingClass+': ' + methodName + '\n')
  tempdiag.write('@enduml \n')
 
 print 'test'
