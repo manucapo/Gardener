4bf1fad42117bbdabf9d36c58769d67d8133f4b6
manucapo <manolotelleria@hotmail.com>
2021-12-01 02:16:08 +0100

    Delete IStringListSender.java

--- src/com/EiriniManu/Messaging/IStringListSender.java
+++
@@ -1,7 +0,0 @@
-package com.EiriniManu.Messaging;
-
-public interface IStringListSender {
-    void addObserver(IMessageObserver observer);
-    void removeObserver(IMessageObserver observer);
-    void sendMessage(Object o);
-}
