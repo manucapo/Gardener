16aea50412f4ec72ca64c0f090387ab83c29b9ab
manucapo <manolotelleria@hotmail.com>
2021-12-15 20:53:53 +0100

    new Test methods + SRS

--- src/com/EiriniManu/Main.java
+++ src/com/EiriniManu/Main.java
@@ -8,9 +8,26 @@ public class Main {
         TestMethod testMethod = new TestMethod();                                                                  // Instantiate a class that provides some simple methods to test the program with.
 
         SequenceDiagramGenerator sequenceDiagramGenerator = new SequenceDiagramGenerator(ParserType.SAFE);            // Instantiate a class that can create a plantUML sequence diagram
+
+
+        // Some extra metadata entered by the user. (NEED TO FIND A BETTER WAY TO PASS THIS)
+       String pathToSource = "src";                                                            // Relative path to TestMethod Class
+        String pathToSequenceDiagram = "Diagrams/SAFE";                                            // Choose desired (relative) path to sequence diagram file
+       String packageName = "com.EiriniManu";                                                 // Package name
+       String className = "TestMethod";                                                       // Class name
+
+       for (int i = 1; i <= 8; i++){
+         String methodName = "test"  + i;
+
+           // Generate plantUML sequence diagram
+           sequenceDiagramGenerator.generateSequenceDiagram(pathToSource ,pathToSequenceDiagram, methodName ,testMethod,className,packageName , String.class, int.class, boolean.class);
+       }
+
+       sequenceDiagramGenerator = new SequenceDiagramGenerator(ParserType.REFLECTION);
+        pathToSequenceDiagram = "Diagrams/REFLECTION";
+
         sequenceDiagramGenerator.addDependency("com.EiriniManu");                                        // Add packages to help resolve classes
         sequenceDiagramGenerator.addDependency("java.lang");
-
         sequenceDiagramGenerator.addDependency("java.util");
         sequenceDiagramGenerator.addDependency("java.util.Optional<T>");
         sequenceDiagramGenerator.addDependency("java.util.Optional");
@@ -21,18 +38,34 @@ public class Main {
         sequenceDiagramGenerator.addDependency("com.github.javaparser.HasParentNode<T>");
         sequenceDiagramGenerator.addDependency("com.github.javaparser.HasParentNode");
 
-        // Some extra metadata entered by the user. (NEED TO FIND A BETTER WAY TO PASS THIS)
-       String pathToSource = "src";                                                            // Relative path to TestMethod Class
-        String pathToSequenceDiagram = "Diagrams";                                            // Choose desired (relative) path to sequence diagram file
-       String packageName = "com.EiriniManu";                                                 // Package name
-       String className = "TestMethod";                                                       // Class name
+        for (int i = 9; i <= 13; i++){
+            String methodName = "test"  + i;
 
-       for (int i = 1; i <= 27; i++){
-         String methodName = "test"  + i;
+            // Generate plantUML sequence diagram
+            sequenceDiagramGenerator.generateSequenceDiagram(pathToSource ,pathToSequenceDiagram, methodName ,testMethod,className,packageName , String.class, int.class, boolean.class);
+        }
 
-           // Generate plantUML sequence diagram
-           sequenceDiagramGenerator.generateSequenceDiagram(pathToSource ,pathToSequenceDiagram, methodName ,testMethod,className,packageName , String.class, int.class, boolean.class);
-       }
+        sequenceDiagramGenerator = new SequenceDiagramGenerator(ParserType.BLOCK);
+        pathToSequenceDiagram = "Diagrams/BLOCK";
+
+        for (int i = 19; i <= 23; i++){
+            String methodName = "test"  + i;
+
+            // Generate plantUML sequence diagram
+            sequenceDiagramGenerator.generateSequenceDiagram(pathToSource ,pathToSequenceDiagram, methodName ,testMethod,className,packageName , String.class, int.class, boolean.class);
+        }
+
+        sequenceDiagramGenerator = new SequenceDiagramGenerator(ParserType.DEEP);
+        pathToSequenceDiagram = "Diagrams/DEEP";
+
+        for (int i = 24; i <=27; i++){
+            String methodName = "test"  + i;
+
+            // Generate plantUML sequence diagram
+            sequenceDiagramGenerator.generateSequenceDiagram(pathToSource ,pathToSequenceDiagram, methodName ,testMethod,className,packageName , String.class, int.class, boolean.class);
+        }
+
     }
+
 }
 
--- src/com/EiriniManu/TestMethod.java
+++ src/com/EiriniManu/TestMethod.java
@@ -5,12 +5,17 @@ package com.EiriniManu;
 */
 
 import com.EiriniManu.IO.DiagramStructure;
+import com.EiriniManu.Parsing.NodeExplorer.CatchNodeExplorer;
 import com.EiriniManu.Parsing.NodeExplorer.MethodNodeExplorer;
+import com.EiriniManu.Parsing.NodeExplorer.NodeExplorerFactory;
+import com.EiriniManu.Parsing.NodeExplorer.ParameterNodeExplorer;
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.expr.NameExpr;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class TestMethod {
 
@@ -24,83 +29,91 @@ public class TestMethod {
         methodTargetStack.add("TEST");
         // TODO CONSTRUCTOR
     }
-
-    public int test1(String s, int i, boolean bool) {
-        this.test2("2", 1, true);
+    // SAFE PARSER TESTS-------------------------------------------------------------------------------------------------------------------------------------------
+    public int test1(String s, int i, boolean bool) {             // The parser can parse single methods and attribute them to the target
+        test1("2", 1, true);
         return i;
     }
 
     public int test2(String s, int i, boolean bool) {
-        System.out.println("test");
-
-        this.test1("2", 1, true);
-        return 26;
+        DiagramStructure.getInstance();                    // The parser can resolve call targets using scope information
+        return i;
     }
 
-    public int test3(String s, int i, boolean bool) {
-        explorer.checkNode(null);
-        System.out.println("test");
+    public int test3(String s, int i, boolean bool) {               // the parser can correctly identify the order of method calls
+        test1("2", 1, true);
+        this.test2("2", 1, true);
+        test3("2", 1, true);
         return 2;
     }
 
 
-    public String test4(String s, int i, boolean bool) {
-        s.split("\\.");
-        return "2";
+    public String test4(String s, int i, boolean bool) {                // the safe parser ignores calls that are not made by the current project package
+        test1("2", 1, true);
+        Math.abs(2);                       // ignored
+        DiagramStructure.getInstance();
+        test3("2", 1, true);
+        return "s";
     }
 
     public String test5(String s, int i, boolean bool) {
-        String f = "TEST F";
-        f.split("\\.");
+        test1( test4("2", 1, true), 1, true);     // the parser can deal with method that are passed as arguments of other methods and still identify the correct call order
         return "2";
     }
 
-    public String test6(String s, int i, boolean bool) {
-        String[] x = {"hes", "test"};
-        x.clone();
+    public String test6(String s, int i, boolean bool) {         // the parser can resolve the call target of fields defined in the current class
+        explorer.checkNode(null);
         return "2";
     }
 
-    public String test7(String s, int i, boolean bool) {
-        Math.abs(2);
+    public String test7(CatchNodeExplorer n, int i, boolean bool) {         // the parser can resolve the call target of arguments passed to the original method call
+        n.checkNode(null);
         return "2";
     }
 
-    public String test8(String s, int i, boolean bool) {
-        test7("a", 2, true).split("\\.");
-        return "2";
+    public String test8(String s, int i, boolean bool) {           // the parser can resolve the call target of fields defined inside the original method call
+        ParameterNodeExplorer n = (ParameterNodeExplorer) NodeExplorerFactory.create(Parameter.class);
+        n.checkNode(null);
+        return "";
     }
 
-    public String test9(String s, int i, boolean bool) {
-        this.test7("a", 2, true).split("\\.");
-        return "2";
+    // REFLECTION PARSER TESTS-------------------------------------------------------------------------------------------------------------------------------------------
+    public String test9(String s, int i, boolean bool) {         // the reflection parser can resolve the target of calls made by packages other than the current project package.
+        Math.abs(2);                                             // (user must provide the external dependencies)
+        return "";
     }
 
 
-    public String test10(String s, int j, boolean bool) {
+    public String test10(String s, int j, boolean bool) {     // the reflection parser can resolve the target of calls made using field acess expressions (i.e System.out)
         System.out.println("if testprint");
-        Math.abs(2);
         return "26";
     }
 
-    public String test11(String s, int j, boolean bool) {
+    public String test11(String s, int j, boolean bool) {    // the reflection parser can recognize and correctly resolve  container classes
 
-        System.out.println(s.toString());
+        int[] ints = {0,2};
+        ints.clone();
+
+        List<String> strings = new ArrayList<>();
+        strings.get(0);
+
+        Optional<Integer> i;
+        i =Optional.of((Integer) 2);
         return "26";
     }
 
-    public String test12(String s, int j, boolean bool) {
+    public String test12(String s, int j, boolean bool) {            // the reflection parser can resolve the target of calls made by objects inside container classes
+        String str = "HeLLO";
+        str.split("")[0].toString();
+        return "26";
+    }
 
+    public String test13(String s, int j, boolean bool) {              // the reflection parser can deal with containers in deeply nested calls
         System.out.println(s.toString().split("").clone().toString());
         return "26";
     }
 
-    public String test13(String s, int j, boolean bool) {
-        s.toString().split("").clone().toString();
-        return "26";
-    }
-
-    public String test14(String s, int j, boolean bool) {
+    public String test14(String s, int j, boolean bool) {              // the reflection parser can resolve symbols declared inside of catch blocks
         try {
             DiagramStructure e = DiagramStructure.getInstance();
             str.split("");
@@ -110,7 +123,7 @@ public class TestMethod {
         return "26";
     }
 
-    public String test15(String s, int j, boolean bool) {
+    public String test15(String s, int j, boolean bool) {          // the reflection parser can differentiate symbols declared inside of catch blocks to those declared outside of them
         try {
             String e = "";
             e.toString();
@@ -120,23 +133,7 @@ public class TestMethod {
         return "26";
     }
 
-    public String test16(String s, int j, boolean bool) {
-
-        Node node = null;
-        node.findFirst(NameExpr.class).isPresent();
-
-        return "26";
-    }
-
-    public String test17(String s, int j, boolean bool) {
-
-        Node node = null;
-        node.findAncestor(NameExpr.class).isPresent();
-
-        return "26";
-    }
-
-    public String test18(String s, int j, boolean bool) {        // LIMITATION: RESOLVING THE TYPE OF GENERICS IS SOMETIMES IMPOSSIBLE
+    public String test16(String s, int j, boolean bool) {    // the reflection parser can SOMETIMES resolve generic methods from scope context
         Node node = null;
         if (node.findAncestor(NameExpr.class).isPresent()) {
             s.equals(node.findAncestor(NameExpr.class).get());
@@ -145,7 +142,8 @@ public class TestMethod {
         return "26";
     }
 
-    public String test19(String s, int j, boolean bool) {
+    public String test17(String s, int j, boolean bool) { // the reflection parser CAN NOT resolve generic types without any further context information
+
         Node node = null;
         if (node.findAncestor(NameExpr.class).isPresent()) {
             s.equals(node.findAncestor(NameExpr.class).get().toString());
@@ -154,81 +152,37 @@ public class TestMethod {
         return "26";
     }
 
-    public String test20(String s, int j, boolean bool) {
+    public String test18(String s, int j, boolean bool) {        // the reflection parser can resolve types using different scope types even inside deeply nested calls.
         String f = "2";
         System.out.println(test1(s.toString(), Integer.parseInt(f), bool));
         return "26";
     }
 
-    public String test21(String s, int j, boolean bool) {
-        return "26";
-    }
-
-    public String test22(String s, int j, boolean bool) {
+    // BLOCK PARSER TESTS-------------------------------------------------------------------------------------------------------------------------------------------
+    public String test19(String s, int j, boolean bool) {  // the block parser can identify if blocks
         if (true) {
             test1("s", 2, true);
         }
         return "26";
     }
 
-    public String test23(String s, int j, boolean bool) {
-        if (true) {
+    public String test20(String s, int j, boolean bool) {   // the block parser can identify for blocks
+        for (int b = 0; b < 2; b++) {
             test1("s", 2, true);
-            if (true) {
-                test1("s", 2, true);
-                if (true) {
-                    test5("s", 2, true);
-                    test6("s", 2, true);
-                    if (true) {
-                        test5("s", 2, true);
-                        test6("s", 2, true);
-                    }
-                }
-                if (true) {
-                    test5("s", 2, true);
-                    test6("s", 2, true);
-                }
-            }
         }
-        test1("s", 2, true);
-        if (true) {
-            test2(test4("s", 2, true), 2, true);
-            if (true) {
-                test1("s", 2, true);
-                if (true) {
-                    test1("s", 2, true);
-                    if (true) {
-                        test5("s", 2, true);
-                        test6("s", 2, true);
-                        if (true) {
-                            test5("s", 2, true);
-                            test6("s", 2, true);
-                        }
-                    }
-                    if (true) {
-                        test5("s", 2, true);
-                        test6("s", 2, true);
-                    }
-                }
-            }
+        return "";
+    }
+
+    public String test21(String s, int j, boolean bool) {    // the block parser can identify while blocks
+        int cnt = 0;
+        while (cnt < 2) {
             test1("s", 2, true);
-            if (true) {
-                test5("s", 2, true);
-                test6("s", 2, true);
-            }
+            cnt++;
         }
-        test12("s", 2, true);
-        if (true) {
-            test5("s", 2, true);
-            test6("s", 2, true);
-        }
-        test6("s", 2, true);
-        test6("s", 2, true);
-        test6("s", 2, true);
         return "26";
     }
 
-    public String test24(String s, int j, boolean bool) {
+    public String test22(String s, int j, boolean bool) {  // the block parser can identify  nested blocks
         if (true) {
             test1("s", 2, true);
             if (true) {
@@ -243,91 +197,12 @@ public class TestMethod {
         return "26";
     }
 
-    public String test25(String s, int j, boolean bool) {
-        if (true) {
-            test1("s", 2, true);
-
-        }
-        test3("s", 2, true);
-        if (true) {
-            test3("s", 2, true);
-        }
-        test3("s", 2, true);
-        return "26";
-    }
-
-    public String test26(String s, int j, boolean bool) {
-        test3("s", 2, true);
-        if (true) {
-            test1("s", 2, true);
-            for (int i = 0; i < 2; i++) {
-                test3("s", 2, true);
-            }
-
-        }
-        test3("s", 2, true);
-        for (int i = 0; i < 2; i++) {
-            test3("s", 2, true);
-            if (true) {
-                test1("s", 2, true);
-                for (int f = 0; f < 2; f++) {
-                    test3("s", 2, true);
-                }
-
-            }
-        }
-        test3("s", 2, true);
-        return "26";
-    }
-
-    public String test27(String s, int j, boolean bool) {
+    public String test23(String s, int j, boolean bool) {      // the block parser can identify DEEPLY  nested blocks of different kinds
         test3("s", 2, true);
         for (int b = 0; b < 2; b++) {
 
-
-        int x = 0;
-        while (x < 2){
-            if (true) {
-                test1("s", 2, true);
-                for (int i = 0; i < 2; i++) {
-                    test3("s", 2, true);
-                }
-                int k = 0;
-                while (k < 10) {
-                    test3("s", 2, true);
-                    if (true) {
-                        test1("s", 2, true);
-                        for (int i = 0; i < 2; i++) {
-                            test3("s", 2, true);
-                        }
-                        k++;
-                    }
-                }
-                test8("s", 2, true);
-                for (int i = 0; i < 2; i++) {
-                    test3("s", 2, true);
-                    if (true) {
-                        test1("s", 2, true);
-                        for (int f = 0; f < 2; f++) {
-                            test3("s", 2, true);
-                        }
-
-                    }
-                }
-                test3("s", 2, true);
-
-            }
-            test25("s", 2, true);
-            x++;
-        }
-        test25("s", 2, true);
-        test25("s", 2, true);
-        if(true) {
-            test25("s", 2, true);
-        }
-        x = 0;
-        while (x < 2) {
-            while (x < 2) {
+            int x = 0;
+            while (x < 2){
                 if (true) {
                     test1("s", 2, true);
                     for (int i = 0; i < 2; i++) {
@@ -361,10 +236,75 @@ public class TestMethod {
                 test25("s", 2, true);
                 x++;
             }
+            test25("s", 2, true);
+            test25("s", 2, true);
+            if(true) {
+                test25("s", 2, true);
+            }
+            x = 0;
+            while (x < 2) {
+                while (x < 2) {
+                    if (true) {
+                        test1("s", 2, true);
+                        for (int i = 0; i < 2; i++) {
+                            test3("s", 2, true);
+                        }
+                        int k = 0;
+                        while (k < 10) {
+                            test3("s", 2, true);
+                            if (true) {
+                                test1("s", 2, true);
+                                for (int i = 0; i < 2; i++) {
+                                    test3("s", 2, true);
+                                }
+                                k++;
+                            }
+                        }
+                        test8("s", 2, true);
+                        for (int i = 0; i < 2; i++) {
+                            test3("s", 2, true);
+                            if (true) {
+                                test1("s", 2, true);
+                                for (int f = 0; f < 2; f++) {
+                                    test3("s", 2, true);
+                                }
+
+                            }
+                        }
+                        test3("s", 2, true);
+
+                    }
+                    test25("s", 2, true);
+                    x++;
+                }
+            }
         }
-        }
+        test3("s", 2, true);
         return "26";
     }
 
+    // DEEP PARSER TESTS-------------------------------------------------------------------------------------------------------------------------------------------
+    public String test24(String s, int j, boolean bool) {   // the deep parser can parse recursively nested methods ( currently only methods declared on the same class )
+        test25("s", 2, true);
+        return "26";
+    }
+
+    public String test25(String s, int j, boolean bool) {
+            test24("s", 2, true);
+        return "26";
+    }
+
+    public String test26(String s, int j, boolean bool) {  // the deep parser correctly recognizes the order of calls even if deeply recursively nested
+        test27("s", 2, true);
+        DiagramStructure.getInstance();
+        return "26";
+    }
+
+    public String test27(String s, int j, boolean bool) {
+        structure.getMethodCaller();
+        test26("s", 2, true);
+        explorer.checkNode(null);
+        return "26";
+    }
 
 }
